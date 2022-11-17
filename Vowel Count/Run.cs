e2f554ffcc7acef5107a0bc5479601a7535feee7
chunchia.liang <chunchia.liang61@titansoft.com.sg>
2022-11-17 14:01:13 +0800

    first commit

---
+++ Vowel Count/Run.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Vowel_Count;
+namespace Kata
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(VowelCount.VowelCounter("I'm Jack. Hello."));
+        }
+    }
+}
---
+++ Vowel Count/Vowel Count.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Vowel_Count;
+
+static class VowelCount
+{
+    public static int VowelCounter(string data)
+    {
+        char[] dataSplit = data.ToLower().ToCharArray();
+        int countResult = 0;
+        foreach (var info in dataSplit)
+        {
+            if (info == 'a' || info == 'e' || info == 'i' || info == 'o' || info == 'u')
+            {
+                countResult++;
+            }
+        }
+
+        return countResult;
+    }
+}
